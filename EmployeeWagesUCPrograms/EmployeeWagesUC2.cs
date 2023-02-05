c8d8136aa82dfac73987f4a69010d15f28d068ac
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-02-05 22:00:53 +0530

    Modified to Employee Wages program

--- EmployeeWagesUCPrograms/EmployeeWagesUC2.cs
+++ EmployeeWagesUCPrograms/EmployeeWagesUC2.cs
@@ -6,16 +6,19 @@ using System.Threading.Tasks;
 
 namespace EmployeeWagesUCPrograms
 {
+    //Created class for Employee UC2 
     public class EmployeeWagesUC2
     {
+        //Created method for Employee UC2
         public static void EmployeeUC2(int empCheck1)
         {
+            //Initialize inputs
             int IS_PRESENT = 1;
             int EMP_RATE_PER_HOUR = 20;
-
             int empHrs = 0;
             int empWages = 0;
 
+            //if statement to check condition
             if (empCheck1 == IS_PRESENT)
             {
                 empHrs = 8;
