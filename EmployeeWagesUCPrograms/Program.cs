69c2664b0f480183f52a9af85466b547b3ba6fe6
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-02-05 20:25:43 +0530

    Modifeid to Employee Wages Program

--- EmployeeWagesUCPrograms/Program.cs
+++ EmployeeWagesUCPrograms/Program.cs
@@ -7,9 +7,26 @@ namespace EmployeeWagesUCPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wages Prgrams");
-            Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            EmployeeWagesUC1.EmployeeUC1(empCheck);
+            Console.WriteLine("Please enter option given below");
+            Console.WriteLine("1:Employee is present or absent UC1\n2:Employee Wage UC2");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Random random = new Random();
+                    int empCheck = random.Next(0, 2);
+                    EmployeeWagesUC1.EmployeeUC1(empCheck);
+                    break;
+                case 2:
+                    Random random1 = new Random();
+                    int empCheck1 = random1.Next(0, 2);
+                    EmployeeWagesUC2.EmployeeUC2(empCheck1);
+                    break;
+                default:
+                    Console.WriteLine("Please enter right option");
+                    break;
+
+            }
         }
     }
 }
