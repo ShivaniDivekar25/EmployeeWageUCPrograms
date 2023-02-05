20c8c0b91739a48cec96f794903883dce640880e
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-02-05 22:10:38 +0530

    Modified to Employee Wages program

---
+++ EmployeeWagesUCPrograms/EmployeeWagesUC4.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWagesUCPrograms
+{
+    //Created class EmployeewagesUC4
+    internal class EmployeeWagesUC4
+    {
+        //Created method EmployeeUC4
+        public static void EmployeeUC4(int empCheck2)
+        {
+            //Initialize input
+            int IS_FULLTIME = 1;
+            int IS_PARTTIME = 2;
+            int EMP_RATE_PER_HRS = 20;
+
+            int empHrs = 0;
+            int empWages = 0;
+
+            //If statement to check conditions
+            if (empCheck2 == IS_FULLTIME)
+            {
+                empHrs = 8;
+                Console.WriteLine("Employee is present and doing Fulltime");
+            }
+            else if (empCheck2 == IS_PARTTIME)
+            {
+                empHrs = 4;
+                Console.WriteLine("Employee is present and doing Parttime");
+            }
+            else
+            {
+                empHrs = 0;
+                Console.WriteLine("Employee is absent");
+            }
+            empWages = empHrs * EMP_RATE_PER_HRS;
+            Console.WriteLine("Employee wages:" + empWages);
+
+        }
+    }
+}
--- EmployeeWagesUCPrograms/Program.cs
+++ EmployeeWagesUCPrograms/Program.cs
@@ -8,7 +8,7 @@ namespace EmployeeWagesUCPrograms
         {
             Console.WriteLine("Welcome to Employee Wages Prgrams");
             Console.WriteLine("Please enter option given below");
-            Console.WriteLine("1:Employee is present or absent UC1\n2:Employee Wage UC2\n3:Employee wage UC3");
+            Console.WriteLine("1:Employee is present or absent UC1\n2:Employee Wage UC2\n3:Employee wage UC3\n4:Employee wage UC4");
             int option = Convert.ToInt32(Console.ReadLine());
             //Switch case to call different method 
             switch (option)
@@ -27,6 +27,11 @@ namespace EmployeeWagesUCPrograms
                     Random random2 = new Random();
                     int empCheck2 = random2.Next(0, 3);
                     EmployeeWagesUC3.EmployeeUC3(empCheck2);
+                    break;
+                case 4:
+                    Random random3 = new Random();
+                    int empCheck3 = random3.Next(0, 3);
+                    EmployeeWagesUC4.EmployeeUC4(empCheck3);
                     break;
                 default:
                     Console.WriteLine("Please enter right option");
