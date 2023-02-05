0fc9e958bdee144cb6be8cdfef762bad387781be
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-02-05 19:56:38 +0530

    Added Welcome to Employee Wages program

---
+++ EmployeeWagesUCPrograms/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeWagesUCPrograms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wages Prgrams");
+        }
+    }
+}
